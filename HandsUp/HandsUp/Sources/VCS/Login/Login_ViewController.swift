79e97b9558c28ae66e0134f51d28a72211d5b2eb
jaesang <jaesang00@konkuk.ac.kr>
2023-01-07 05:21:38 +0900

    feat : PW visible 버튼, 로그인 에러 경고 UI 함수 구현

--- HandsUp/HandsUp/Sources/VCS/Login/Login_ViewController.swift
+++ HandsUp/HandsUp/Sources/VCS/Login/Login_ViewController.swift
@@ -1,29 +1,28 @@
-//
-//  Login_Login_ViewController.swift
-//  HandsUp
-//
-//  Created by 황재상 on 2023/01/07.
-//
-
 import UIKit
 
 class Login_ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var IDTextField_Login: UITextField!
+    @IBOutlet weak var PWTextField_Login: UITextField!
+    @IBOutlet weak var emailBox_Login: RoundedShadow_UIButton!
+    @IBOutlet weak var loginErrorMark_Login: UIImageView!
+    
+    @IBAction func setPWSecureTextEntry_Login(_ sender: Any) {
+        if(PWTextField_Login.isSecureTextEntry){
+            PWTextField_Login.isSecureTextEntry = false
+        }
+        else{
+            PWTextField_Login.isSecureTextEntry = true
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loginErrorAlert_Login(){
+        emailBox_Login.layer.borderWidth = 1
+        emailBox_Login.layer.borderColor = UIColor(named: "HandsUpRed")?.cgColor
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginErrorMark_Login.alpha = 0
+        
+    }
 }
