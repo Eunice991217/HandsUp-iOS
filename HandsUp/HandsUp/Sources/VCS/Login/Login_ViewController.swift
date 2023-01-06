79b2fd2dfe11280b5d1a58f85dbbf3e599ff9585
jaesang <jaesang00@konkuk.ac.kr>
2023-01-07 05:38:26 +0900

    feat : ID,PW 입력시 로그인 버튼 활성화

--- HandsUp/HandsUp/Sources/VCS/Login/Login_ViewController.swift
+++ HandsUp/HandsUp/Sources/VCS/Login/Login_ViewController.swift
@@ -5,6 +5,7 @@ class Login_ViewController: UIViewController {
     @IBOutlet weak var PWTextField_Login: UITextField!
     @IBOutlet weak var emailBox_Login: RoundedShadow_UIButton!
     @IBOutlet weak var loginErrorMark_Login: UIImageView!
+    @IBOutlet weak var loginButton_Login: RoundedShadow_UIButton!
     
     @IBAction func setPWSecureTextEntry_Login(_ sender: Any) {
         if(PWTextField_Login.isSecureTextEntry){
@@ -12,6 +13,14 @@ class Login_ViewController: UIViewController {
         }
         else{
             PWTextField_Login.isSecureTextEntry = true
+        }
+    }
+    
+    @objc func isIDPWInput(_sender: Any?){
+        if(IDTextField_Login.text != "" && PWTextField_Login.text != ""){
+            loginButton_Login.backgroundColor = UIColor(named: "HandsUpOrange")
+        }else{
+            loginButton_Login.backgroundColor = UIColor(named: "HandsUpWhiteGrey")
         }
     }
     
@@ -24,5 +33,7 @@ class Login_ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loginErrorMark_Login.alpha = 0
+        self.IDTextField_Login.addTarget(self, action: #selector(self.isIDPWInput(_sender:)), for: .editingChanged)
+        self.PWTextField_Login.addTarget(self, action: #selector(self.isIDPWInput(_sender:)), for: .editingChanged)
     }
 }
