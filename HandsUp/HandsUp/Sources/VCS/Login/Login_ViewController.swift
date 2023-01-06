179cc10f5430a2bae9ab6ae9f5f4cf95a5164cae
jaesang <jaesang00@konkuk.ac.kr>
2023-01-07 07:04:12 +0900

    feat : 로그인오류 알림해제 구현

--- HandsUp/HandsUp/Sources/VCS/Login/Login_ViewController.swift
+++ HandsUp/HandsUp/Sources/VCS/Login/Login_ViewController.swift
@@ -7,6 +7,7 @@ class Login_ViewController: UIViewController {
     @IBOutlet weak var loginErrorMark_Login: UIImageView!
     @IBOutlet weak var loginButton_Login: RoundedShadow_UIButton!
     var isLoginEnable_Login: Bool = false
+    var isLoginError_Login: Bool = false
     
     @IBAction func setPWSecureTextEntry_Login(_ sender: Any) {
         if(PWTextField_Login.isSecureTextEntry){
@@ -23,6 +24,10 @@ class Login_ViewController: UIViewController {
     }
     
     @objc func isIDPWInput(_sender: Any?){
+        if(isLoginError_Login){
+            disableLoginErrorAlert_Login()
+        }
+        
         if(IDTextField_Login.text != "" && PWTextField_Login.text != ""){
             loginButton_Login.backgroundColor = UIColor(named: "HandsUpOrange")
             self.isLoginEnable_Login = true
@@ -36,18 +41,32 @@ class Login_ViewController: UIViewController {
     @IBAction func login_Login(_ sender: Any) {
         if(self.isLoginEnable_Login){
             //이메일 로그인 실행
+            if(false){
+                
+            }else{
+                loginErrorAlert_Login()
+            }
         }
     }
     
     func loginErrorAlert_Login(){
+        isLoginError_Login = true
         loginErrorMark_Login.alpha = 1
         emailBox_Login.layer.borderWidth = 1
-        emailBox_Login.layer.borderColor = UIColor(named: "HandsUpRed")?.cgColor
+        IDTextField_Login.textColor = UIColor(named: "HandsUpRed")
+    }
+    
+    func disableLoginErrorAlert_Login(){
+        isLoginError_Login = false
+        loginErrorMark_Login.alpha = 0
+        emailBox_Login.layer.borderWidth = 0
+        IDTextField_Login.textColor = UIColor(named: "HandsUpGrey")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loginErrorMark_Login.alpha = 0
+        emailBox_Login.layer.borderColor = UIColor(named: "HandsUpRed")?.cgColor
         self.IDTextField_Login.addTarget(self, action: #selector(self.isIDPWInput(_sender:)), for: .editingChanged)
         self.PWTextField_Login.addTarget(self, action: #selector(self.isIDPWInput(_sender:)), for: .editingChanged)
         //self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font : UIFont(name: <#T##String#>, size: 18)
