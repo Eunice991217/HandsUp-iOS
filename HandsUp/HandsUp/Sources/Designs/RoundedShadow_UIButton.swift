e4b3eeca8648bcbfa3e056c7f3f03f6c5d8927dc
jaesang <jaesang00@konkuk.ac.kr>
2023-01-07 01:32:28 +0900

    feat : 로그인_첫화면 UI 구현

---
+++ HandsUp/HandsUp/Sources/Designs/RoundedShadow_UIButton.swift
@@ -0,0 +1,52 @@
+//
+//  RoundedShadow_UIButton.swift
+//  HandsUp
+//
+//  Created by 황재상 on 2023/01/07.
+//
+
+import UIKit
+
+class RoundedShadow_UIButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor = UIColor.gray{
+        didSet{
+            self.layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float = 1.0 {
+        didSet{
+            self.layer.shadowOpacity = shadowOpacity
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize = CGSize.zero {
+        didSet{
+            self.layer.shadowOffset = shadowOffset
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 6 {
+        didSet{
+            self.layer.shadowRadius = shadowRadius
+        }
+    }
+}
---
+++ HandsUp/HandsUp/Sources/VCS/Login_FirstView_ViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class Login_FirstView_ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
