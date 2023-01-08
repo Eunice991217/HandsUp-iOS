bc8b26a9be21ce8f1c454289bdcd3f38742b6ca0
jaesang <jaesang00@konkuk.ac.kr>
2023-01-08 21:02:27 +0900

    feat : UIView 흔들림 함수 추가

--- HandsUp/HandsUp/Sources/Designs/RoundedShadow_UIView.swift
+++ HandsUp/HandsUp/Sources/Designs/RoundedShadow_UIView.swift
@@ -54,8 +54,8 @@ class RoundedShadow_UIView: UIView {
     func shake() {
             let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
             animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
-            animation.duration = 0.6
-            animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
+            animation.duration = 0.3
+            animation.values = [-7.5, 7.5, -7.5, 7.5, -5.0, 5.0, -2.5, 2.5, 0.0 ]
             layer.add(animation, forKey: "shake")
         }
 }
