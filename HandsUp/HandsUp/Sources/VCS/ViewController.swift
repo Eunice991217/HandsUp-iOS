4c3f43b14b56cf9030662bd9088f5507261ef629
Eunice991217 <org9899@naver.com>
2023-01-09 18:55:59 +0900

    feat : 홈 화면 추가

--- HandsUp/HandsUp/Sources/VCS/ViewController.swift
+++ HandsUp/HandsUp/Sources/VCS/ViewController.swift
@@ -2,18 +2,25 @@
 //  ViewController.swift
 //  HandsUp
 //
-//  Created by 황재상 on 2023/01/05.
+//  Created by 김민경 on 2023/01/05.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var HomeTabView: UIView!
+    @IBOutlet weak var HomeRestartBtn: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        HomeTabView.clipsToBounds = true
+        HomeTabView.layer.cornerRadius = 40
+        HomeTabView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
+        
+        HomeRestartBtn.layer.cornerRadius=13
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
