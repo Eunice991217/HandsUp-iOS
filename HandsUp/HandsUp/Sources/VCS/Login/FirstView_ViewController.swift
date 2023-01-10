dca7f2095aadb04c9182273e62970cff8fe7d89a
jaesang <jaesang00@konkuk.ac.kr>
2023-01-11 06:05:30 +0900

    fix : 로그인 첫화면 UI 구조 변경

--- HandsUp/HandsUp/Sources/VCS/Login/FirstView_ViewController.swift
+++ HandsUp/HandsUp/Sources/VCS/Login/FirstView_ViewController.swift
@@ -2,13 +2,45 @@ import UIKit
 
 class FirstView_ViewController: UIViewController {
     
+    @IBOutlet weak var loginKakaoButton_FV: UIButton!
+    @IBOutlet weak var kakaoIcon_FV: UIButton!
+    @IBOutlet weak var kakaoTitle_FV: UIButton!
+    @IBOutlet weak var loginAppleButton_FV: UIButton!
+    @IBOutlet weak var appleIcon_FV: UIButton!
+    @IBOutlet weak var appleTitle_FV: UIButton!
+    @IBOutlet weak var loginGoogleButton_FV: UIButton!
+    @IBOutlet weak var googleIcon_FV: UIButton!
+    @IBOutlet weak var googleTitle_FV: UIButton!
+    
+    func buttonInit_FV(){
+      
+    }
+    @objc func loginKakaoHighlightToggle_FV(_sender: Any){
+        kakaoIcon_FV.isHighlighted.toggle()
+        kakaoTitle_FV.isHighlighted.toggle()
+    }
+    
+    @objc func loginAppleHighlightToggle_FV(_sender: Any){
+        appleIcon_FV.isHighlighted.toggle()
+        appleTitle_FV.isHighlighted.toggle()
+    }
+    
+    @objc func loginGoogleHighlightToggle_FV(_sender: Any){
+        googleIcon_FV.isHighlighted.toggle()
+        googleTitle_FV.isHighlighted.toggle()
+    }
+    
     @IBAction func loginGoogleButton_FV(_ sender: Any) {
     }
     
     @IBAction func loginAppleButton_FV(_ sender: Any) {
         
     }
-    @IBAction func loginKakaoButton_FV(_ sender: Any) {
+    @IBAction func loginKakaoButtonTap_FV(_ sender: Any) {
+        
+        
+    }
+    @IBAction func loginKakaoButtonTouchOut_FV(_ sender: Any) {
         
     }
     
@@ -16,7 +48,21 @@ class FirstView_ViewController: UIViewController {
         self.performSegue(withIdentifier: "showLoginEmail", sender: self)
     }
     
+    func buttonHighlightInit_FV(){
+        loginKakaoButton_FV.addTarget(self, action: #selector(loginKakaoHighlightToggle_FV(_sender: )), for: .touchDown)
+        loginKakaoButton_FV.addTarget(self, action: #selector(loginKakaoHighlightToggle_FV(_sender: )), for: .touchUpInside)
+        loginKakaoButton_FV.addTarget(self, action: #selector(loginKakaoHighlightToggle_FV(_sender: )), for: .touchUpOutside)
+        loginAppleButton_FV.addTarget(self, action: #selector(loginAppleHighlightToggle_FV(_sender: )), for: .touchDown)
+        loginAppleButton_FV.addTarget(self, action: #selector(loginAppleHighlightToggle_FV(_sender: )), for: .touchUpInside)
+        loginAppleButton_FV.addTarget(self, action: #selector(loginAppleHighlightToggle_FV(_sender: )), for: .touchUpOutside)
+        loginGoogleButton_FV.addTarget(self, action: #selector(loginGoogleHighlightToggle_FV(_sender: )), for: .touchDown)
+        loginGoogleButton_FV.addTarget(self, action: #selector(loginGoogleHighlightToggle_FV(_sender: )), for: .touchUpInside)
+        loginGoogleButton_FV.addTarget(self, action: #selector(loginGoogleHighlightToggle_FV(_sender: )), for: .touchUpOutside)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonHighlightInit_FV()
     }
 }
+
